e0ae5c1c26667993b2c7f6ad2773515de87e5930
IronKinoko <kinoko_main@outlook.com>
2020-07-17 00:25:26 +0800

    feat: 优化首页显示，更新app图标

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -21,10 +21,8 @@ class MyDocument extends Document {
           />
           <meta name="theme-color" content="#000" />
           <link rel="apple-touch-icon" href="/static/icon.png" />
-          <meta
-            name="apple-mobile-web-app-status-bar-style"
-            content="white"
-          ></meta>
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black" />
           <link rel="manifest" href="/static/manifest.json" />
         </Head>
         <body>
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-// import Link from 'next/link'
+import React, { useEffect } from 'react'
 import Layout from '../components/Layout'
-import { GetServerSideProps } from 'next'
 import { useRouter } from 'next/router'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 import GalleryList from '@/index/GalleryList'
+import Router from 'next/router'
 const useStyles = makeStyles((theme) =>
   createStyles({
     search: { margin: theme.spacing(2, 'auto') },
@@ -22,13 +21,12 @@ const IndexPage = () => {
   )
 }
 
-export default IndexPage
-
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  if (!req.headers.cookie?.includes('ipb_member_id')) {
-    res.statusCode = 302
-    res.setHeader('Location', '/signin')
-    res.end()
+export default () => {
+  if (typeof window !== 'undefined') {
+    if (!document.cookie.includes('ipb_member_id')) {
+      Router.push('/signin')
+      return null
+    }
   }
-  return { props: {} }
+  return <IndexPage />
 }
